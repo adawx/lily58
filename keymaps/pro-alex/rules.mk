80c5a0e283e675fbabea6afc1e46f2f242026965
Alex Dawkes <alex@dawkes.org>
2022-08-16 21:43:52 +0800

    ocean animation test

--- keymaps/pro-alex/rules.mk
+++ keymaps/pro-alex/rules.mk
@@ -9,6 +9,8 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
+OLED_DRIVER = SSD1306
+WPM_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
@@ -18,3 +20,18 @@ SRC +=  ./lib/rgb_state_reader.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
+	
+ifeq ($(strip $(OLED_ENABLE)), yes)
+    #... your code here...
+
+    ifdef OCEAN_DREAM_ENABLE
+        ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+            SRC += ocean_dream.c
+            OPT_DEFS += -DOCEAN_DREAM_ENABLE
+        endif
+    endif
+    ifndef OCEAN_DREAM_ENABLE
+        SRC += ocean_dream.c
+        OPT_DEFS += -DOCEAN_DREAM_ENABLE
+    endif
+endif
